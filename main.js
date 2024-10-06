f8f9c705c971be775cb28b9b058510eed4d04125
Danila Udalchikov <githubs69@gmail.com>
2024-10-06 10:47:25 +0300

    fix count

--- main.js
+++ main.js
@@ -1,6 +1,199 @@
-// Что я должен успеть решить за 3 часа
-//
-// 1) Сверстать блок корзины с 3 позициями
-// 2) Сделать это все в pixel-perfect
-// 3) Сделать классы по методологии БЭМ
-//
+let parentCheckbox = document.querySelector('.main-checkbox') // главный чекбокс продуктов
+let childCheckboxs = document.querySelectorAll('.checkbox-child') // дочерние чекбосы продуктов
+let basketResult = document.querySelector('.basket__result'); // результат корзины
+let arrowCartProduct = document.querySelector('.cart-svg__margin'); // кнопка которая скрывает и отображает продукты
+let arrowCartMissing = document.querySelector('.arrowCartMissing');
+
+let products = document.querySelector('.products__gap'); // блок с продуктами
+
+let checkboxText = document.querySelector('.checkbox__text');
+let checkboxCart = document.getElementById('checkbox');
+let productMissing = document.querySelector('.product__hidden');
+let countBasket = document.querySelector('.count-basket p')
+let productNavigation = document.querySelectorAll('.product__navigation')
+
+const totalPriceWrapper = document.getElementById('total-price') // итоговое число
+
+const quantiyProducts = document.getElementById('quantity-products'); // место в которое нужно вставить количество всех продуктов
+
+const sumOldProducts = document.getElementById('sum-Old_Products') // сумма всех продуктов по старой цене
+
+function numberWithSpaces(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " "); // регулярка которая делит числа на разряды для удобства и читаемости больших сумм
+}
+
+const ACTION = {
+	PLUS: 'plus',
+	MINUS: 'minus'
+}
+
+const getItemSubTotalPrice = (inputs) => Math.floor(Number(inputs.value) * Number(inputs.dataset.price)) // сокращенная версия 
+
+const getItemOldTotalPrice = (inputs) => Math.floor(Number(inputs.value) * Number(inputs.dataset.oldprice))
+
+
+// выводит основное итоговое число со всеми правками в соответстующее поле 
+const setTotalPrice = (value) => { 
+	totalPriceWrapper.textContent = numberWithSpaces(value);
+	// вывод итогового числа всех товаров с применением регулярки
+	totalPriceWrapper.dataset.value = value;
+	// вместо того чтобы отформатировать в итоге к числу строку, мы просто сделали это более коротким способом
+}
+
+const setOldTotalPrice = (value) => {
+	sumOldProducts.textContent = numberWithSpaces(value);
+	sumOldProducts.dataset.value = value;
+}
+
+const init = () => {
+	let totalCost = 0; // начальное значение итоговой суммы
+	let oldTotalCost = 0; // начальное значение итоговой суммы по старым ценникам
+	let productItems = document.querySelectorAll('.product__item_active'); // все карточки товаров
+
+
+
+	productItems.forEach((basketItem) => {
+		totalCost += getItemSubTotalPrice(basketItem.querySelector('.inputs'))
+		oldTotalCost += getItemOldTotalPrice(basketItem.querySelector('.inputs'))
+		// проходимся циклом по всему массиву из трех товаров, преобразуем их в число после обращаемся к инпутам, а именно в value, позже умножаем также на значение инпутов только дата атрибута price, в котором указана его цена\
+	})
+
+		setTotalPrice(totalCost);
+		setOldTotalPrice(oldTotalCost);
+}
+
+
+const calculateSeparateItem = (basketItem, action) => {
+	const input = basketItem.querySelector('.inputs') // нашли инпут в котором все значения мы вычисляем
+
+	switch (action) {
+		case ACTION.PLUS: // обращаемся к объекту действие: плюс
+			input.value++; // увеливаем значение value на один
+
+			setTotalPrice(Math.trunc(Number(totalPriceWrapper.dataset.value)) + Math.trunc(Number(input.dataset.price)))
+			// quantiyProducts.textContent = 
+			setOldTotalPrice(Math.trunc((Number(sumOldProducts.dataset.value))) + Math.trunc(Number(input.dataset.oldprice)))
+
+			break; // заканчиваем проверку
+
+		case ACTION.MINUS: // обращаемся к объекту действие: минус
+			input.value--; // уменьшаем значение value на один
+
+			setTotalPrice(Math.trunc(Number(totalPriceWrapper.dataset.value)) - Math.trunc(Number(input.dataset.price)))
+
+			setOldTotalPrice(Math.trunc(Number(sumOldProducts.dataset.value)) - Math.trunc(Number(input.dataset.oldprice)))
+			break; // заканчиваем проверку
+	}
+
+	basketItem.querySelector('.subtotal').textContent = `${numberWithSpaces(getItemSubTotalPrice(input))} сом` // вывод подытога обычной цены
+
+	basketItem.querySelector('.subtotal-old').textContent = `${numberWithSpaces(getItemOldTotalPrice(input))} сом` // вывод подытога старой цены
+}
+
+products.addEventListener('click', (event) => {
+	const target = event.target
+
+	if (target.classList.contains('plus')) {
+	 calculateSeparateItem(
+		target.closest('.product__item_active'), 
+		ACTION.PLUS
+	)
+	 // ищет родителя
+	}
+	if (target.classList.contains('minus')) {
+		calculateSeparateItem(
+			target.closest('.product__item_active'), 
+			ACTION.MINUS
+		)
+	}
+})
+
+init();
+
+// проверяет если убрал дочерний чекбокс то активность главного меняется на false
+function checkCheckbox() {
+	for (let i = 0; i < childCheckboxs.length; i++) {
+		childCheckboxs[i].addEventListener('click', () => {
+			if (childCheckboxs[i].checked == false) {
+				parentCheckbox.checked = false;
+			} 
+		})
+	}
+}
+
+checkCheckbox()
+
+ // прячет текст выбрать все и отображает кол-во и сумму продуктов
+function hiddenCartText() {
+	checkboxText.textContent = [...document.querySelectorAll('.product__item_active')].length + ' товара  · ' + totalPriceWrapper.textContent + ' сом'
+	checkboxCart.classList.add('none')
+}
+
+// показывает изначальное состояние поля выбрать все
+function showCartText() {
+	checkboxText.textContent = 'Выбрать все'
+	checkboxCart.classList.remove('none')
+}
+
+// прячет карточку с товарами и опускает стрелку вниз
+function hiddenProducts() {
+	arrowCartProduct.classList.add('transform-arrow')
+	products.classList.add('none')
+}
+
+// возвращает карточку с товарами в обычное состояние и стрелку
+function showProducts() {
+	arrowCartProduct.classList.remove('transform-arrow')
+	products.classList.remove('none')
+}
+
+// прячет карточку с отсутствующими товарами и опускает стрелку вниз
+function hiddenMissingProducts() {
+	arrowCartMissing.classList.add('transform-arrow')
+	productMissing.classList.add('none')
+}
+
+// возвращает карточку с отсутствующими товарами в обычное состояние и стрелку
+function showMissingProducts() {
+	arrowCartMissing.classList.remove('transform-arrow')
+	productMissing.classList.remove('none')
+}
+
+// выбирает все чекбоксы по одному главному чекбоксу и также убирает при повторном клике
+function selectCheckBox() {
+	for (let i = 0; i < childCheckboxs.length; i++) {
+		if (parentCheckbox.checked == true) {
+			childCheckboxs[i].checked = true;
+		} else {
+			childCheckboxs[i].checked = false;
+		}
+	} 
+}
+
+parentCheckbox.addEventListener('click', () => {
+	selectCheckBox()
+})
+
+arrowCartProduct.addEventListener('click', () => {
+	for (let i = 0; i < [...document.querySelectorAll('.product__item_active')].length; i++) {
+
+	if (!arrowCartProduct.classList.contains('transform-arrow')) {
+		hiddenProducts()
+		hiddenCartText()
+	} else  {
+		showProducts()
+		showCartText()
+	}
+}
+})
+
+arrowCartMissing.addEventListener('click', () => {
+	if (!arrowCartMissing.classList.contains('transform-arrow')) {
+		hiddenMissingProducts();
+	} else {
+		showMissingProducts();
+	}
+})
+
+
+// когда мы нажимаем на минус или плюс элемента, мы должны подняться до родителя и внутри него уже искать количество, цену, подсумму и тд.
